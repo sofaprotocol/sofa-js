60182e64b5361940aa7b3630d64179ac06f054a0
Fay Montage <accounts@faymontage.com>
2017-03-07 14:05:03 +0100

    Add tests for Payment value handling

---
+++ test/SOFA.spec.js
@@ -0,0 +1,44 @@
+const expect = require('chai').expect
+const assert = require('chai').assert
+const TestMessages = require('./TestMessages')
+const BN = require('bn.js')
+const numberToBN = require('number-to-bn')
+
+const SOFA = require('../sofa/SOFA')
+
+describe('SOFA', () => {
+  describe('#Payment()', () => {
+    let np = SOFA.Payment(TestMessages.NUMERIC_PAYMENT);
+    let nsp = SOFA.Payment(TestMessages.NUMERIC_STRING_PAYMENT);
+    let bnp = SOFA.Payment(TestMessages.BN_PAYMENT);
+    let vp = SOFA.Payment(TestMessages.VALID_PAYMENT);
+
+    it('should handle value as number', () => {
+      assert.isTrue(BN.isBN(np.value))
+      assert.equal(np.content.value, "0xf4240")
+    })
+
+    it('should handle value as numeric string', () => {
+      assert.isTrue(BN.isBN(nsp.value))
+      assert.equal(nsp.content.value, "0xf4240")
+    })
+
+    it('should handle value as BN', () => {
+      assert.isTrue(BN.isBN(bnp.value))
+      assert.equal(bnp.content.value, "0xf4240")
+    })
+
+    it('should handle value as hex', () => {
+      assert.isTrue(vp.value.eq(numberToBN(1000000)))
+      assert.equal(vp.content.value, "0xf4240")
+    })
+
+    it('should serialize value as hex regardless of input type', () => {
+      assert.equal(JSON.parse(np.json).value, '0xf4240')
+      assert.equal(JSON.parse(nsp.json).value, '0xf4240')
+      assert.equal(JSON.parse(bnp.json).value, '0xf4240')
+      assert.equal(JSON.parse(vp.json).value, '0xf4240')
+    })
+
+  })
+})
